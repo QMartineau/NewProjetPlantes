0d553e3d10b496806913cea548c9fe8abb065fce
QMartineau <86412064+QMartineau@users.noreply.github.com>
2021-10-18 16:58:57 +0200

    ajout crud plantes

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -3,8 +3,12 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use App\Repository\PlantesRepository;
 use Symfony\Component\Routing\Annotation\Route;
+use Doctrine\ORM\EntityManagerInterface;
+use App\Entity\Plantes;
 
 class HomeController extends AbstractController
 {
@@ -13,6 +17,20 @@ class HomeController extends AbstractController
     {
         return $this->render('home/index.html.twig', [
             'controller_name' => 'HomeController',
+        ]);
+    }
+
+    #[Route('/home/{id}', name: 'home_single')]
+    public function showSingleEvent(int $id , PlantesRepository $plantesRepository): Response
+    {
+        
+        $Plantes = $plantesRepository->find($id) ;
+        if (!$Plantes) {
+            throw $this->createNotFoundException('La table est vide');
+        }
+            
+        return $this->render('home/index.html.twig', [
+            'Plantes' => $Plantes
         ]);
     }
 }
