b62ba5a1d0a993009bea4611ff9e4cadc458b227
Bonseg86 <86412145+Bonseg86@users.noreply.github.com>
2021-10-18 08:57:16 +0200

    test bdd

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -5,6 +5,8 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use App\Entity\Plantes;
+use App\Repository\PlantesRepository;
 
 class HomeController extends AbstractController
 {
@@ -15,4 +17,20 @@ class HomeController extends AbstractController
             'controller_name' => 'HomeController',
         ]);
     }
+
+    #[Route('/home/{id}', name: 'home_single')]
+    public function showSingleEvent(int $id , PlantesRepository $PlantesRepository): Response
+    {
+        
+        $Image = $PlantesRepository->find($id) ;
+        if (!$Image) {
+            throw $this->createNotFoundException('La table est vide');
+        }
+            
+        return $this->render('home/single.html.twig', [
+            'Image' => $Image
+        ]);
+    }
 }
+
+
