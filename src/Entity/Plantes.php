eb5fd4a5c21b66d61a75d1b374e07594e809ea91
QMartineau <86412064+QMartineau@users.noreply.github.com>
2021-10-21 17:08:18 +0200

    ajout données dans plantes

--- src/Entity/Plantes.php
+++ src/Entity/Plantes.php
@@ -42,6 +42,16 @@ class Plantes
      */
     private $description;
 
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $nom;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $image;
+
     public function getId(): ?int
     {
         return $this->id;
@@ -121,6 +131,30 @@ class Plantes
     public function setDescription(string $description): self
     {
         $this->description = $description;
+
+        return $this;
+    }
+
+    public function getNom(): ?string
+    {
+        return $this->nom;
+    }
+
+    public function setNom(string $nom): self
+    {
+        $this->nom = $nom;
+
+        return $this;
+    }
+
+    public function getImage(): ?string
+    {
+        return $this->image;
+    }
+
+    public function setImage(string $image): self
+    {
+        $this->image = $image;
 
         return $this;
     }
