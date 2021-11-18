87cf07f3f8e12b09f30148780f4960bddf2cb6d2
QMartineau <86412064+QMartineau@users.noreply.github.com>
2021-11-18 16:57:56 +0100

    modif page accueil format tel

--- public/script/index.js
+++ public/script/index.js
@@ -5,3 +5,13 @@ function myFunction2() {
 function myFunction() {  
     document.getElementById("masked").style.display = "block";
 }
+
+window.onscroll = function() {displayNone()};
+
+function displayNone() {
+    if (document.body.scrollTop > 1500 || document.documentElement.scrollTop > 1500) {
+        document.getElementById("gotobot").className = "displaynone";
+    } else {
+        document.getElementById("gotobot").className = "";
+    }
+}
