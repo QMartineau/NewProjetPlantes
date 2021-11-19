6b0f10a57a53700d34af5efac139eb6242652b0a
QMartineau <86412064+QMartineau@users.noreply.github.com>
2021-11-19 11:59:23 +0100

    modif gototop et gotobot

--- public/script/index.js
+++ public/script/index.js
@@ -6,7 +6,7 @@ function myFunction() {
     document.getElementById("masked").style.display = "block";
 }
 
-window.onscroll = function() {displayNone()};
+window.onscroll = function() {displayNone(), scrollFunction()};
 
 function displayNone() {
     if (document.body.scrollTop > 1500 || document.documentElement.scrollTop > 1500) {
@@ -14,4 +14,16 @@ function displayNone() {
     } else {
         document.getElementById("gotobot").className = "";
     }
+}
+
+function scrollFunction() {
+    if (document.body.scrollTop > 1500 || document.documentElement.scrollTop > 1500) {
+        document.getElementById("gototop").style.display = "block";
+    } else {
+        document.getElementById("gototop").style.display = "none";
+    }
+}
+
+function topFunction() {
+    document.documentElement.scrollTop = 0;
 }
