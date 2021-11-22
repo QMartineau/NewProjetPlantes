7fdaf6361f43ad9fefbfccf1f12b1b1fa7308fae
QMartineau <86412064+QMartineau@users.noreply.github.com>
2021-11-22 17:00:17 +0100

    gototop et bot fini

--- public/script/index.js
+++ public/script/index.js
@@ -6,23 +6,22 @@ function myFunction() {
     document.getElementById("masked").style.display = "block";
 }
 
-window.onscroll = function() {displayNone(), scrollFunction()};
+window.onscroll = function() {displayNone()};
 
 function displayNone() {
+
     if (document.body.scrollTop > 1500 || document.documentElement.scrollTop > 1500) {
+
         document.getElementById("gotobot").className = "displaynone";
+        document.getElementById("gototop").className = "";
+        
     } else {
+
         document.getElementById("gotobot").className = "";
+        document.getElementById("gototop").className = "displaynone";
     }
 }
 
-function scrollFunction() {
-    if (document.body.scrollTop > 1500 || document.documentElement.scrollTop > 1500) {
-        document.getElementById("gototop").style.display = "block";
-    } else {
-        document.getElementById("gototop").style.display = "none";
-    }
-}
 
 function topFunction() {
     document.documentElement.scrollTop = 0;
