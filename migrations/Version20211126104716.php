2d18057c9fb6f7eaa919b4e3c8703b06e9f891d3
Bonseg86 <86412145+Bonseg86@users.noreply.github.com>
2021-11-26 11:57:18 +0100

    utoo

--- migrations/Version20211126104716.php
+++ migrations/Version20211126104716.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20211019114017 extends AbstractMigration
+final class Version20211126104716 extends AbstractMigration
 {
     public function getDescription(): string
     {
@@ -20,12 +20,12 @@ final class Version20211019114017 extends AbstractMigration
     public function up(Schema $schema): void
     {
         // this up() migration is auto-generated, please modify it to your needs
-        $this->addSql('ALTER TABLE plantes ADD nom VARCHAR(255) NOT NULL');
+        $this->addSql('DROP TABLE quiz');
     }
 
     public function down(Schema $schema): void
     {
         // this down() migration is auto-generated, please modify it to your needs
-        $this->addSql('ALTER TABLE plantes DROP nom');
+        $this->addSql('CREATE TABLE quiz (id INT AUTO_INCREMENT NOT NULL, intitule VARCHAR(255) CHARACTER SET utf8mb4 NOT NULL COLLATE `utf8mb4_unicode_ci`, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8 COLLATE `utf8_unicode_ci` ENGINE = InnoDB COMMENT = \'\' ');
     }
 }
