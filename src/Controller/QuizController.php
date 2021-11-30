39ad707763f40a556fb967ec80587ccbcd64541a
Bonseg86 <86412145+Bonseg86@users.noreply.github.com>
2021-11-30 17:00:13 +0100

    sto

--- src/Controller/QuizController.php
+++ src/Controller/QuizController.php
@@ -4,6 +4,7 @@ namespace App\Controller;
 
 use App\Entity\Reponses;
 use App\Repository\QuestionsRepository;
+use App\Repository\ReponsesRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -11,18 +12,40 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class QuizController extends AbstractController
 {
-    #[Route('/quiz', name: 'quiz')]
-    public function index( QuestionsRepository $questionsRepository): Response
+    #[Route('/quiz/{id}', name: 'quiz')]
+    public function index( int $id , QuestionsRepository $questionsRepository, ReponsesRepository $reponsesRepository): Response
     {
-        $Qest = $questionsRepository->findAll(); 
-        dd($Qest);
+        
+        $Ques = $questionsRepository->findAll($id) ;
+        $Cultis = $reponsesRepository->findby(array('question' => $id)) ;
+
+        $tab_rep = [];
+
+        foreach($Cultis as $item)
+        {
+            $tab_rep [] = $item->getIntitule();
+        }
+        // $Qest = $questionsRepository->findAll(); 
+        
+
+
+        // dd($tab_rep);
+        if (!$Cultis) {
+            throw $this->createNotFoundException('La table est vide');
+        }
+        if (!$Ques) {
+            throw $this->createNotFoundException('La table est vide');
+        }
+        
         //Crer une liste de questions 
         //pour chaque question que tu vas trouver dans ta base de données 
         //tu vas aller chercher toute les réponse qui sont rataché a cette question
 
         return $this->render('quiz/index.html.twig', [
             'controller_name' => 'QuizController',
-             'quiz' => $Qest,
+            'Ques' => $tab_rep,
+            'quiz' => $Ques,
+            
         ]);
     }
 
