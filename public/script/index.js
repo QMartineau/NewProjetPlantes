aa7a20a33a261dc6e09b8c6ddcaf304023e21c83
QMartineau <86412064+QMartineau@users.noreply.github.com>
2021-12-02 17:05:23 +0100

    full css / js done

--- public/script/index.js
+++ public/script/index.js
@@ -9,18 +9,43 @@
 window.onscroll = function () { displayNone() };
 
 function displayNone() {
+    if (window.matchMedia("(max-width: 767px)").matches) {
+        if (document.body.scrollTop > 550 || document.documentElement.scrollTop > 550) {
 
-    if (document.body.scrollTop > 550 || document.documentElement.scrollTop > 550) {
-
-        document.getElementById("gotobot").className = "displaynone";
-        document.getElementById("gototop").className = "";
-        document.getElementById("navbar").className = "displaynone";
+            document.getElementById("gotobot").className = "displaynone";
+            document.getElementById("gototop").className = "";
+            document.getElementById("navbar").className = "displaynone";
         
-    } else {
+        } else {
 
-        document.getElementById("gotobot").className = "";
-        document.getElementById("gototop").className = "displaynone";
-        document.getElementById("navbar").className = "navbar navbar-expand-lg navbar-light position-fixed w-100";
+            document.getElementById("gotobot").className = "";
+            document.getElementById("gototop").className = "displaynone";
+            document.getElementById("navbar").className = "navbar navbar-expand-lg navbar-light fixed-top w-100";
+        }
+    } else if (window.matchMedia("(min-width: 767px)").matches && window.matchMedia("(max-width: 1023px)").matches) {
+
+        if (document.body.scrollTop > 525 || document.documentElement.scrollTop > 525) {
+
+            document.getElementById("gotobot").className = "displaynone";
+            document.getElementById("gototop").className = "";
+            document.getElementById("navbar").className = "displaynone";
+        
+        } else {
+
+            document.getElementById("gotobot").className = "";
+            document.getElementById("gototop").className = "displaynone";
+            document.getElementById("navbar").className = "navbar navbar-expand-lg navbar-light sticky-top w-100";
+        }
+    } else if (window.matchMedia("(min-width: 1024px)").matches) {
+
+        if (document.body.scrollTop > 500 || document.documentElement.scrollTop > 500) {
+
+            document.getElementById("gototop").className = "";
+        
+        } else {
+
+            document.getElementById("gototop").className = "displaynone";
+        }
     }
 }
 
--- public/script/plantes.js
+++ public/script/plantes.js
@@ -1,16 +1,41 @@
-window.onscroll = function() {displayNone()};
+// function myFunction2() {
+//     document.getElementById("masked").style.display = "none";
+// }
+
+// function myFunction() {  
+//     document.getElementById("masked").style.display = "block";
+// }
+
+window.onscroll = function () { displayNone() };
 
 function displayNone() {
+    if (window.matchMedia("(max-width: 767px)").matches) {
+        if (document.body.scrollTop > 550 || document.documentElement.scrollTop > 550) {
 
-    if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
-
-        document.getElementById("gototop").className = "";
-        document.getElementById("navbar").className = "displaynone";
+            document.getElementById("gotobot").className = "displaynone";
+            document.getElementById("gototop").className = "";
+            document.getElementById("navbar").className = "displaynone";
         
-    } else {
+        } else {
 
-        document.getElementById("gototop").className = "displaynone";
-        document.getElementById("navbar").className = "navbar navbar-expand-lg navbar-light position-fixed w-100";
+            document.getElementById("gotobot").className = "";
+            document.getElementById("gototop").className = "displaynone";
+            document.getElementById("navbar").className = "navbar navbar-expand-lg navbar-light sticky-top w-100";
+        }
+    } else if (window.matchMedia("(min-width: 768px)").matches) {
+
+        if (document.body.scrollTop > 525 || document.documentElement.scrollTop > 525) {
+
+            document.getElementById("gotobot").className = "displaynone";
+            document.getElementById("gototop").className = "";
+            document.getElementById("navbar").className = "displaynone";
+        
+        } else {
+
+            document.getElementById("gotobot").className = "";
+            document.getElementById("gototop").className = "displaynone";
+            document.getElementById("navbar").className = "navbar navbar-expand-lg navbar-light sticky-top w-100";
+        }
     }
 }
 
