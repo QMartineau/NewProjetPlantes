288bcdc73d0bf8731332020e16aee45ef44b07a9
QMartineau <86412064+QMartineau@users.noreply.github.com>
2021-12-09 17:00:33 +0100

    à ça d'être fini

--- public/script/index.js
+++ public/script/index.js
@@ -4,28 +4,28 @@ function displayNone() {
     if (window.matchMedia("(max-width: 767px)").matches) {
         if (document.body.scrollTop > 550 || document.documentElement.scrollTop > 550) {
 
-            document.getElementById("gotobot").className = "displaynone";
+            document.getElementById("gotobot").className = "d-none";
             document.getElementById("gototop").className = "";
-            document.getElementById("navbar").className = "displaynone";
+            document.getElementById("navbar").className = "d-none";
         
         } else {
 
             document.getElementById("gotobot").className = "";
-            document.getElementById("gototop").className = "displaynone";
+            document.getElementById("gototop").className = "d-none";
             document.getElementById("navbar").className = "navbar navbar-expand-lg navbar-light fixed-top w-100";
         }
     } else if (window.matchMedia("(min-width: 767px)").matches && window.matchMedia("(max-width: 1023px)").matches) {
 
         if (document.body.scrollTop > 525 || document.documentElement.scrollTop > 525) {
 
-            document.getElementById("gotobot").className = "displaynone";
+            document.getElementById("gotobot").className = "d-none";
             document.getElementById("gototop").className = "";
-            document.getElementById("navbar").className = "displaynone";
+            document.getElementById("navbar").className = "d-none";
         
         } else {
 
             document.getElementById("gotobot").className = "";
-            document.getElementById("gototop").className = "displaynone";
+            document.getElementById("gototop").className = "d-none";
             document.getElementById("navbar").className = "navbar navbar-expand-lg navbar-light sticky-top w-100";
         }
     } else if (window.matchMedia("(min-width: 1024px)").matches) {
@@ -36,7 +36,7 @@ function displayNone() {
         
         } else {
 
-            document.getElementById("gototop").className = "displaynone";
+            document.getElementById("gototop").className = "d-none";
         }
     }
 }
